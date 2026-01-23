980f540fc6175dad2fb25f019ca5c06debf43306
adotri-frimpong <goldmalcom43@gmail.com>
2026-01-23 13:09:14 +0100

    [FEAT] Update of airflow-volumes/dags/ETL_DAG.py file : Silver Transformation task tests

--- airflow-volumes/dags/ETL_DAG.py
+++ airflow-volumes/dags/ETL_DAG.py
@@ -46,5 +46,6 @@ with DAG(
         python_callable=etl_functions.transform_bronze_data_to_silver
     )
 
-    checking_raw_json_files_existence >> extract >> [transform, remove_local_files]
+    checking_raw_json_files_existence >> extract >> remove_local_files
+    transform
     # extract >> transform
