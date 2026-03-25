520195a2b1f64355666557113265ce51c07295f8
adotri-frimpong <goldmalcom43@gmail.com>
2026-03-25 02:34:55 +0100

    [FEAT] Update of trino-volumes/sql/create_bronze_iceberg_table.sql file

--- trino-volumes/sql/create_bronze_iceberg_table.sql
+++ trino-volumes/sql/create_bronze_iceberg_table.sql
@@ -1,18 +1,19 @@
 
 CREATE TABLE IF NOT EXISTS minio_warehouse.test_schema.test_table (
-    transaction_id VARCHAR,
-    transaction_date TIMESTAMP,
-    client_name VARCHAR,
-    customer_loyalty_member BOOLEAN,
-    basket_items_count INTEGER,
-    basket_items_product_name VARCHAR,
-    basket_items_quantity INTEGER,
-    basket_items_unit_price DOUBLE,
-    basket_items_total_amount DOUBLE,
-    total_amount DOUBLE,
-    currency VARCHAR,
-    payment_method VARCHAR,
-    ingestion_date DATE
+    transaction_id VARCHAR COMMENT 'The unique identifier for each transaction.',
+    transaction_date TIMESTAMP COMMENT 'The date and time when the transaction occurred.',
+    client_name VARCHAR COMMENT 'The name of the client who made the transaction.',
+    customer_loyalty_member BOOLEAN COMMENT 'The status of the client, loyal member or not.',
+    basket_items_count INTEGER COMMENT 'Total number of distinct product name.',
+    basket_items_product_name VARCHAR COMMENT 'The name of the product that was purchased in the transaction.',
+    basket_items_quantity INTEGER COMMENT 'Total quantity of purchased product for the specific product name.',
+    basket_items_unit_price DOUBLE COMMENT 'The unit price of the product that was purchased in the transaction.',
+    basket_items_total_amount DOUBLE COMMENT 'The total amount of the purchased product_name (calculated as basket_items_quantity * basket_items_unit_price).',
+    total_amount DOUBLE COMMENT 'The sum of basket_items_total_amount for all purchased basket_items_product_name.',
+    currency VARCHAR COMMENT 'The currency in which the transaction was made (e.g., USD, EUR, etc.).',
+    payment_method VARCHAR COMMENT 'The method of payment used for the transaction (e.g., credit card, cash, etc.).',
+    ingestion_date DATE COMMENT 'The date when the transaction data was ingested into the bronze table.'
 )
+    COMMENT 'Bronze Iceberg table which retrieves raw ingested data.'
     WITH ( partitioning = ARRAY['ingestion_date'], 
-        format = 'PARQUET');
+           format = 'PARQUET');
